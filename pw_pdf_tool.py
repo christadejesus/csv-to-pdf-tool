48f3fa2a9840422cf4983ca094024860af5880d7
christadejesus <christa.tech@outlook.com>
2025-01-16 09:11:13 -0500

    Add user prompt loop and checkFileType function.
    Within a while True loop, user is prompted for the file name to convert.
    The file name is passed into the checkFileType function along with
    the .csv extension as args. checkFileType is a reusable function that
    accepts any file extension as an arg, returns True if valid and False if
    invalid. If invalid, it also returns information to the user.
    
    If checkFileType returns True, the program continues. If False, user
    will be prompted again to enter the file name.
    
    KeyboardInterrupt error handling and a sys.exit on successful
    program completion also added.

--- pw_pdf_tool.py
+++ pw_pdf_tool.py
@@ -19,6 +19,28 @@ def main():
     """ Display program purpose """
     print("Welcome to the PW PDF Tool!\nLet's quickly convert your exported password manager CSV\nto a clean, customized, and printable PDF.\n")
 
+    while True:
+            try:
+                """ Prompt user for filename """
+                file_name = input("Enter the name of your CSV file (e.g., pw_export.csv): ").strip()
+                if checkFileType(file_name, ".csv"):
+                    print("So far so good!")
+                    break
+
+            except KeyboardInterrupt:
+                """ On Ctrl+C, exit """
+                sys.exit(Fore.CYAN + "\nHave an awesome day!\n")
+
+    sys.exit(Fore.CYAN + "\nHave an awesome day!\n")
+
+""" Validate file type """
+def checkFileType(file_name, ext):
+    if file_name.endswith(ext) == True:
+        print(Fore.GREEN + f"✔ {ext} file")
+        return True
+    else:
+        print(Fore.YELLOW + f"Not a valid file.\nPlease check the file name and try again.\n")
+        return False
 
 if __name__ == "__main__":
     main()
