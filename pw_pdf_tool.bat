c9eb08e142a9b72850019f499ec9ead946986a90
christadejesus <christa.tech@outlook.com>
2025-01-17 20:20:21 -0500

    Add sample batch script in pw_pdf_tool.bat file

---
+++ pw_pdf_tool.bat
@@ -0,0 +1,5 @@
+:: This is a sample batch script file, or .bat file, for quick program execution.::
+:: Don't forget to change the name and/or path to match your chosen file location.::
+
+@echo off
+python C:\Users\<your_name>\Scripts\pw_pdf_tool.py %*
